9cda991ea1d714ccd8b7e8a50e5146aed86efd6c
Abhi-Coder722 <072abhi@gmail.com>
2023-11-02 18:34:54 +0100

    Fix typography

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,7 +78,7 @@ const Works = () => {
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
 
           class projects()  &#123;<br/>
-                    /*Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and lve demos in it.
+                    /*Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it.
           It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively. */  
           <br/>
           <br/>
